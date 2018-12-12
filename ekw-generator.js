53ed985090b72eafe49e55fe180c54cfbb134b70
Thomas Bieniek <thomas.bieniek@gmail.com>
2018-12-12 19:56:09 +0100

    add dynamic folder-name

--- ekw-generator.js
+++ ekw-generator.js
@@ -1,13 +1,15 @@
 // Filesystem by Node.js
 const fs = require('fs');
 
+// write your EKW-Name here
 var ekwName = "test";
 
 const handleEkwName = (ekwName) => {
 	console.log('test...');
 	console.log(ekwName);
+	return ekwName;
 }
 
 var correctEkwName = handleEkwName(ekwName);
 
-fs.mkdirSync("correctEkwName");
+fs.mkdirSync(correctEkwName);
