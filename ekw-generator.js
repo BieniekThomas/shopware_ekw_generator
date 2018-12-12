08365f04074ad2c3409364d30c0f9fe971928c64
Thomas Bieniek <thomas.bieniek@gmail.com>
2018-12-12 20:05:15 +0100

    get rid of umlaute

--- ekw-generator.js
+++ ekw-generator.js
@@ -2,11 +2,21 @@
 const fs = require('fs');
 
 // write your EKW-Name here
-var ekwName = "test";
+var ekwName = "täst";
+var description = "";
 
 const handleEkwName = (ekwName) => {
-	console.log('test...');
-	console.log(ekwName);
+	// Stuff happens here
+    ekwName = ekwName.toLowerCase();
+  	ekwName = ekwName.replace(/ä/g, 'ae');
+  	ekwName = ekwName.replace(/ö/g, 'oe');
+  	ekwName = ekwName.replace(/ü/g, 'ue');
+  	ekwName = ekwName.replace(/ß/g, 'ss');
+  	ekwName = ekwName.replace(/ /g, '-');
+  	ekwName = ekwName.replace(/\./g, '');
+  	ekwName = ekwName.replace(/,/g, '');
+  	ekwName = ekwName.replace(/\(/g, '');
+  	ekwName = ekwName.replace(/\)/g, '');
 	return ekwName;
 }
 
