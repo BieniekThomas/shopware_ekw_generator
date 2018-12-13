cdd6dd4b89e49edfaa64aad7bb3bb333a6f8896e
Thomas Bieniek <thomas.bieniek@econsor.de>
2018-12-13 17:11:48 +0100

    add complete folder-structure

--- ekw-generator.js
+++ ekw-generator.js
@@ -9,9 +9,9 @@ const fs = require('fs');
 const handleEkwName = require('./handleEkwName');
 const makeOtherCase = require('./makeOtherCase');
 
-
 var correctEkwName = handleEkwName.handleEkwName(ekwName);
-correctEkwName = makeOtherCase.makeOtherCase(ekwName);
+var fileEkwName = handleEkwName.handleEkwName(ekwName);
+fileEkwName = makeOtherCase.makeOtherCase(fileEkwName);
 
 // make plugin-containing folder
 fs.mkdirSync('Shopware_Plugins/'+correctEkwName, { recursive: true });
@@ -19,25 +19,14 @@ fs.mkdirSync('Shopware_Plugins/'+correctEkwName, { recursive: true });
 // Direct to Inital Folder
 process.chdir('Shopware_Plugins/'+correctEkwName);
 
-// Generate FolderStructure
-// Bootstrap-Folder
-fs.mkdir('Bootstrap/', (err) => {
-  	if (err) throw err;
-  	console.log('Bootstrap Folder Saved!');
-});
-
-fs.mkdir('Resources/', (err) => {
-  	if (err) throw err;
-  	console.log('Resources Folder Saved!');
-});
-
-fs.mkdir('Subscriber/', (err) => {
-  	if (err) throw err;
-  	console.log('Subscriber Folder Saved!');
-});
+// Generate FolderStructure Synchronously -> mkdir is async
+fs.mkdirSync('Bootstrap/');
+fs.mkdirSync('Resources/views/emotion_components/widgets/emotion/components/', { recursive: true });
+fs.mkdirSync('Resources/views/frontend/_public/src/less/', { recursive: true });
+fs.mkdirSync('Subscriber/');
 
 // Direct to Bootstrap
-/*process.chdir('Bootstrap/');*/
+process.chdir('Bootstrap/');
 fs.writeFile('EmotionElementInstaller.php', 'EmotionElementInstaller.php Content', function (err) {
 	if (err) throw err;
   	console.log('EmotionElementInstaller.php Saved!');
