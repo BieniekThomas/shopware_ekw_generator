d8f29dd13fe83077a885bd5bc2403d26a8742f01
Thomas Bieniek <thomas.bieniek@econsor.de>
2018-12-13 18:19:51 +0100

    making src_textfiles + adding function for replacing string

--- ekw-generator.js
+++ ekw-generator.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 // Imports and requires
 const handleEkwName = require('./handler/handleEkwName');
 const makeOtherCase = require('./handler/makeOtherCase');
+const writeFiles = require('./handler/replaceVariables');
 
 // Input Part //
 // Validate Input
@@ -18,30 +19,35 @@ var fileEkwName = makeOtherCase.makeOtherCase(correctEkwName);
 
 
 // Folder Part //
-// make plugin-containing folder Synchronously
 fs.mkdirSync('_Shopware_Plugins/' + correctEkwName, { recursive: true });
 
 
 // Direct to Inital Folder
 process.chdir('_Shopware_Plugins/' + correctEkwName);
 
-// Generate FolderStructure Synchronously -> mkdir is async
-fs.mkdirSync('ComponentHandler/');
-fs.mkdirSync('Bootstrap/');
-fs.mkdirSync('Resources/views/emotion_components/widgets/emotion/components/', { recursive: true });
-fs.mkdirSync('Resources/views/frontend/_public/src/less/', { recursive: true });
-fs.mkdirSync('Subscriber/');
+	// Generate FolderStructure Synchronously -> mkdir is async
+	fs.mkdirSync('ComponentHandler/');
+	fs.mkdirSync('Bootstrap/');
+	fs.mkdirSync('Resources/views/emotion_components/widgets/emotion/components/', { recursive: true });
+	fs.mkdirSync('Resources/views/frontend/_public/src/less/', { recursive: true });
+	fs.mkdirSync('Subscriber/');
 
 // Direct to Bootstrap
 process.chdir('Bootstrap/');
-fs.writeFile( fileEkwName + '.php', fileEkwName + '.php Content', function (err) {
-	if (err) throw err;
-  	console.log('EmotionElementInstaller.php Saved!');
-});
 
+	// This is not working
+	// var outputFile = writeFiles.replaceVariables(correctEkwName, description, '_initialEmotionPhp.txt');
+
+	fs.writeFile( fileEkwName + '.php', outputFile, function (err) {
+		if (err) throw err;
+	  	console.log('EmotionElementInstaller.php Saved!');
+	});
+
+// Direct to ComponentHandler
 process.chdir('../ComponentHandler');
-fs.writeFile( fileEkwName + 'Handler.php', fileEkwName + '.php Content', function (err) {
-	if (err) throw err;
-  	console.log('ComponentHandler.php Saved!');
-});
+
+	fs.writeFile( fileEkwName + 'Handler.php', fileEkwName + '.php Content', function (err) {
+		if (err) throw err;
+	  	console.log('ComponentHandler.php Saved!');
+	});
 
---
+++ handler/replaceVariables.js
@@ -0,0 +1,11 @@
+const replaceVariables = (correctEkwName, otherInput, fileAsTxt) = {
+	// do something
+	return 	"a very long string";
+}
+
+module.exports = {
+  replaceVariables: function (correctEkwName, otherInput, fileAsTxt) {
+    outputString = replaceVariables(correctEkwName, otherInput, fileAsTxt);
+    return outputString;
+  }
+};
