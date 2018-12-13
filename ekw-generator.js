37afd91343b3de1bcc7092377f5d9f654747f48e
Thomas Bieniek <thomas.bieniek@econsor.de>
2018-12-13 17:25:23 +0100

    improved folders + input-handling

--- ekw-generator.js
+++ ekw-generator.js
@@ -9,17 +9,23 @@ const fs = require('fs');
 const handleEkwName = require('./handleEkwName');
 const makeOtherCase = require('./makeOtherCase');
 
+// Input Part //
+// Validate Input
 var correctEkwName = handleEkwName.handleEkwName(ekwName);
-var fileEkwName = handleEkwName.handleEkwName(ekwName);
-fileEkwName = makeOtherCase.makeOtherCase(fileEkwName);
 
-// make plugin-containing folder
-fs.mkdirSync('Shopware_Plugins/'+correctEkwName, { recursive: true });
+// Generate FileName
+var fileEkwName = makeOtherCase.makeOtherCase(correctEkwName);
+
+
+// Folder Part //
+// make plugin-containing folder Synchronously
+fs.mkdirSync('Shopware_Plugins/' + correctEkwName, { recursive: true });
 
 // Direct to Inital Folder
-process.chdir('Shopware_Plugins/'+correctEkwName);
+process.chdir('Shopware_Plugins/' + correctEkwName);
 
 // Generate FolderStructure Synchronously -> mkdir is async
+fs.mkdirSync('ComponentHandler/');
 fs.mkdirSync('Bootstrap/');
 fs.mkdirSync('Resources/views/emotion_components/widgets/emotion/components/', { recursive: true });
 fs.mkdirSync('Resources/views/frontend/_public/src/less/', { recursive: true });
@@ -27,7 +33,7 @@ fs.mkdirSync('Subscriber/');
 
 // Direct to Bootstrap
 process.chdir('Bootstrap/');
-fs.writeFile('EmotionElementInstaller.php', 'EmotionElementInstaller.php Content', function (err) {
+fs.writeFile( fileEkwName + '.php', fileEkwName + '.php Content', function (err) {
 	if (err) throw err;
   	console.log('EmotionElementInstaller.php Saved!');
 });
