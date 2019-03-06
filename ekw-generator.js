9a80cfd890e44d5fe4f1a7c13e0328ffbb31d648
Thomas Bieniek <thomas.bieniek@econsor.de>
2019-03-06 10:08:11 +0100

    adding all replace-strings + fixed small mistake in services.xml

--- ekw-generator.js
+++ ekw-generator.js
@@ -9,20 +9,22 @@ const handleEkwName = require('./handler/handleEkwName');
 const makeOtherCase = require('./handler/makeOtherCase');
 
 // Inputs
-let ekwName = 'yourEkwName';
-let description = '';
+const inputs = {
+	ekwName: 'defaultEkwName',
+	description: '',
+}
 
 // Console Inputs
-ekwName = readline.question('Write your wanted EKW-Name: ');
-description = readline.question('Short description of your Plugin (recommended): ');
+inputs.ekwName = readline.question('Write your wanted EKW-Name: ');
+inputs.description = readline.question('Short description of your Plugin (recommended): ');
 
 // Validate Input
-var fixedEkwName = handleEkwName.handleEkwName(ekwName);
-var fixedEkwFileName = makeOtherCase.makeOtherCase(ekwName);
+var fixedEkwName = handleEkwName.handleEkwName(inputs.ekwName);
+var fixedEkwFileName = makeOtherCase.makeOtherCase(inputs.ekwName);
 
 // create Folders and Files
 makeDirectoriesAndFiles(fixedEkwName, fixedEkwFileName);
-replaceStrings(fixedEkwName, fixedEkwFileName);
+replaceStrings(fixedEkwName, inputs.description);
 
 
 function readAndWrite (inputPath, outputName, correctEkwName ,outputPath) {
@@ -62,19 +64,55 @@ function makeDirectoriesAndFiles (correctEkwName, correctFileName) {
 	return console.log('Writing Files Done');
 }
 
-function replaceStrings(correctEkwName, correctFileName){
+function replaceStrings(ekwName, description){
+
+	const directoryName = ekwName;
+
+	const plugin_name = makeOtherCase.makeOtherCase(ekwName);
+	const element_template = directoryName + '_two';
+	const element_name = directoryName + '_three';
+	const element_cls = directoryName + '_four';
+	const element_desc = description;
+	const link = directoryName + '_six';
+	const author = directoryName + '_seven';
+	const plugin_mod_name = directoryName + '_eight';
+	const desc_de = description;
+	const desc_en = description + '_eng';
+
 	// replace strings options
 	const pluginNameOptions = {
 		files: [
-			'./_Shopware_Plugins/' + correctEkwName + '/**/*.php',
-			'./_Shopware_Plugins/' + correctEkwName + '/**/*.xml',
-			'./_Shopware_Plugins/' + correctEkwName + '/**/*.js',
-			'./_Shopware_Plugins/' + correctEkwName + '/**/*.tpl',
-			'./_Shopware_Plugins/' + correctEkwName + '/**/*.less',
+			'./_Shopware_Plugins/' + directoryName + '/**/*.php',
+			'./_Shopware_Plugins/' + directoryName + '/**/*.xml',
+			'./_Shopware_Plugins/' + directoryName + '/**/*.js',
+			'./_Shopware_Plugins/' + directoryName + '/**/*.tpl',
+			'./_Shopware_Plugins/' + directoryName + '/**/*.less',
 		],
 		//Replacement to make (string or regex)
-		from: /\[::plugin_name::\]/g,
-		to: correctFileName,
+		from: [
+			/\[::plugin_name::\]/g,
+			/\[::element_template::\]/g,
+			/\[::element_name::\]/g,
+			/\[::element_cls::\]/g,
+			/\[::element_desc::\]/g,
+			/\[::link::\]/g,
+			/\[::author::\]/g,
+			/\[::plugin_mod_name::\]/g,
+			/\[::desc_de::\]/g,
+			/\[::desc_en::\]/g
+		],
+		to: [
+			plugin_name,
+			element_template,
+			element_name,
+			element_cls,
+			element_desc,
+			link,
+			author,
+			plugin_mod_name,
+			desc_de,
+			desc_en
+		],
 	};
 
 	// replace Strings
